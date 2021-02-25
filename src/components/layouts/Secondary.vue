37aeec1a4f462957c9330e343dd4902055e17346
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-02-25 17:21:46 -0300

    Adicionando Layout

---
+++ src/components/layouts/Secondary.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="content">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.content {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
