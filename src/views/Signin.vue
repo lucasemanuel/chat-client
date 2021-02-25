fc19f1532f246840ead0c08aec3cd2eb1198b254
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-02-25 17:24:40 -0300

    Adicionando tela de login

---
+++ src/views/Signin.vue
@@ -0,0 +1,54 @@
+<template>
+  <SecondaryLayout>
+    <form>
+      <input
+        type="text"
+        id="fusername"
+        name="fusername"
+        placeholder="Username"
+      />
+      <input
+        type="text"
+        id="fpassword"
+        name="fpassword"
+        placeholder="Password"
+      />
+      <button type="submit">Login</button>
+      <a href="#">Create account</a>
+    </form>
+  </SecondaryLayout>
+</template>
+
+<script>
+import SecondaryLayout from '@/components/layouts/Secondary'
+
+export default {
+  name: 'Signin',
+  components: {
+    SecondaryLayout
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '../styles/form';
+
+form {
+  display: flex;
+  flex-direction: column;
+  width: 420px;
+  text-align: center;
+
+  input {
+    margin-bottom: 12px;
+  }
+
+  button {
+    margin-top: 12px;
+  }
+
+  a {
+    margin-top: 32px;
+  }
+}
+</style>
