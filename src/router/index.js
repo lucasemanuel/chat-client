2ea246ba229b6456c9d5c0dfef513f5eb09a7986
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-02-25 20:50:41 -0300

    Adicionando rota default

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Signin from '../views/Signin.vue'
+import Signup from '../views/Signup.vue'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/signin',
     name: 'Signin',
     component: Signin
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
   }
 ]
 
