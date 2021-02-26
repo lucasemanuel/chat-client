99e8381b1d891ed1ee614c98955e9f8b0301641f
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-02-26 15:47:09 -0300

    Estilizando mensagens do chat

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <main>
     <header>
-      <h1 class="logo">ChatWebSocket</h1>
+      <h1 class="logo">chatSocket</h1>
     </header>
-    <aside class="users">
+    <aside class="users none">
       <span class="userLogged">@lucasemanuel</span>
       <ul>
         <li>@soteldo</li>
@@ -44,4 +44,52 @@ export default {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+main {
+  header {
+    height: 64px;
+    background: #982ea1;
+    width: 100%;
+    line-height: 64px;
+
+    h1.logo {
+      font-weight: 500;
+      font-size: 28px;
+      text-align: center;
+    }
+  }
+
+  .conversation {
+    padding: 0 10px;
+
+    .messages {
+      display: flex;
+      flex-direction: column;
+
+      .message {
+        display: flex;
+        border-radius: 16px;
+        width: fit-content;
+        padding: 12px;
+
+        &:not(:last-of-type) {
+          margin-bottom: 12px;
+        }
+
+        &.me {
+          background: #ddd;
+          border-bottom-right-radius: 0;
+          text-align: right;
+          align-self: flex-end;
+          margin-left: 12%;
+        }
+
+        &.him {
+          background: royalblue;
+          border-top-left-radius: 0;
+        }
+      }
+    }
+  }
+}
+</style>
