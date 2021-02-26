42ea41119145fa3b4a666c6576943d957a845ae5
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-02-26 15:14:15 -0300

    Criando arquivo dedicado para estilo do app

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,4 @@
   <router-view />
 </template>
 
-<style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap');
-
-body {
-  background: cornflowerblue;
-  height: 100vh;
-}
-
-* {
-  font-family: 'Rubik', sans-serif;
-  padding: 0;
-  margin: 0;
-  outline: none;
-}
-
-a {
-  color: #111;
-}
-</style>
+<style lang="scss" src="@/styles/index.scss" />
