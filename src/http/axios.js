61965f11e06439852eedbb74535ce4eaff52bc33
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-03-03 16:26:29 -0300

    Configurando Axios

--- src/http/axios.js
+++ src/http/axios.js
@@ -2,5 +2,9 @@ import axios from 'axios'
 
 export default axios.create({
   baseURL: process.env.VUE_APP_API_URL,
-  timeout: 1000
+  timeout: 1000,
+  headers: {
+    'content-type': 'application/json',
+    Accept: 'application/json'
+  }
 })
