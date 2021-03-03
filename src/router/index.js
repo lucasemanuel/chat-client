82a1a39f1d08a079f254357a885c90d227d0f71e
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-03-03 19:49:22 -0300

    Adicionando autorização nas rotas

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Signin from '../views/Signin.vue'
 import Signup from '../views/Signup.vue'
 import Home from '../views/Home.vue'
+import store from '../store'
 
 Vue.use(VueRouter)
 
@@ -10,11 +11,12 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: { requiresAuth: true }
   },
   {
     path: '/login',
-    name: 'Signin',
+    name: 'Login',
     component: Signin
   },
   {
@@ -26,6 +28,27 @@ const routes = [
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const auth =
+    store.state.auth !== undefined
+      ? store.state.auth
+      : store.getters.isAuthenticated
+
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!auth) {
+      next({ name: 'Login' })
+    } else {
+      next()
+    }
+  } else {
+    if (auth) {
+      next({ name: 'Home' })
+    } else {
+      next()
+    }
+  }
 })
 
 export default router
