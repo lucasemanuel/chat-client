a32ac048ee6a98a6a692dd878a929f88397172ac
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-03-03 16:30:06 -0300

    Executando logout caso a resposta seja 401

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,16 @@
   <router-view />
 </template>
 
+<script>
+export default {
+  created () {
+    this.$http.interceptors.response.use(undefined, error => {
+      if (error.status === 401) this.$store.dispatch('logout')
+
+      return Promise.reject(error)
+    })
+  }
+}
+</script>
+
 <style lang="scss" src="@/styles/index.scss" />
