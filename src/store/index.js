ca36e358948ab3b534f2489d4b188f5f0618a7ea
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-03-03 16:28:01 -0300

    Adicionando actions de Login e Logout

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,48 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import axios from '@/http/axios'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+  state: {},
+  getters: {
+    token () {
+      return localStorage.getItem('token')
+    },
+    user () {
+      return JSON.parse(localStorage.getItem('user'))
+    }
   },
-  mutations: {
-  },
+  mutations: {},
   actions: {
+    login ({ commit }, user) {
+      return new Promise((resolve, reject) => {
+        axios
+          .post('/auth/login', user)
+          .then(response => {
+            const { user, access_token: token } = response.data
+            localStorage.setItem('token', token)
+            localStorage.setItem('user', JSON.stringify(user))
+            axios.defaults.headers.common = { Authorization: `Bearer ${token}` }
+            resolve(response)
+          })
+          .catch(error => {
+            localStorage.removeItem('token')
+            localStorage.removeItem('user')
+            reject(error)
+          })
+      })
+    },
+    logout ({ commit }) {
+      return new Promise((resolve, reject) => {
+        localStorage.removeItem('token')
+        localStorage.removeItem('user')
+        delete axios.defaults.headers.common.Authorization
+        resolve()
+      })
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
