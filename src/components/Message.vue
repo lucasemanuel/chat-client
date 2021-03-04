a17f02b413c141d1280980e6e60b71dd0bdc9a2a
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-03-04 20:36:14 -0300

    Componentizando mensagens

---
+++ src/components/Message.vue
@@ -0,0 +1,50 @@
+<template>
+  <p class="message" v-bind:class="classMessage">
+    {{ content }}
+  </p>
+</template>
+
+<script>
+export default {
+  name: 'Message',
+  props: {
+    typeUser: Boolean,
+    content: String
+  },
+  computed: {
+    classMessage () {
+      return {
+        me: this.typeUser,
+        him: !this.typeUser
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.message {
+  display: flex;
+  border-radius: 16px;
+  width: fit-content;
+  padding: 12px;
+  background: #ddd;
+
+  &:not(:last-of-type) {
+    margin-bottom: 12px;
+  }
+
+  &.me {
+    background: #ddd;
+    border-bottom-right-radius: 0;
+    text-align: right;
+    align-self: flex-end;
+    margin-left: 12%;
+  }
+
+  &.him {
+    background: royalblue;
+    border-top-left-radius: 0;
+  }
+}
+</style>
---
+++ src/components/Messages.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="messages">
+    <Message
+      v-for="m in messages"
+      v-bind:content="m.body"
+      v-bind:key="m.id"
+      v-bind:type-user="m.source_id == user.id"
+    />
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapState } from 'vuex'
+
+import Message from '@/components/Message'
+export default {
+  name: 'Messages',
+  components: {
+    Message
+  },
+  computed: {
+    ...mapState(['destination', 'messages']),
+    ...mapGetters(['user'])
+  }
+}
+</script>
+
+<style lang="scss">
+.messages {
+  display: flex;
+  flex-direction: column;
+  margin: 18px 0;
+  height: calc(100vh - 64px - 36px - 36px - 52px);
+  overflow: auto;
+  padding: 0 12px;
+}
+</style>
