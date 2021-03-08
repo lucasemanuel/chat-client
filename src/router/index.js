19f9e9db44adf60559dbf02337f571005a4eb993
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-03-08 16:58:35 -0300

    Refatorando autorização de rota

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Signin from '../views/Signin.vue'
-import Signup from '../views/Signup.vue'
-import Home from '../views/Home.vue'
-import store from '../store'
+import Signin from '@/views/Signin.vue'
+import Signup from '@/views/Signup.vue'
+import Home from '@/views/Home.vue'
+import store from '@/store'
 
 Vue.use(VueRouter)
 
@@ -32,22 +32,14 @@ const router = new VueRouter({
 
 router.beforeEach((to, from, next) => {
   const auth =
-    store.state.auth !== undefined
-      ? store.state.auth
+    to.params.auth !== undefined
+      ? to.params.auth
       : store.getters.isAuthenticated
 
   if (to.matched.some(record => record.meta.requiresAuth)) {
-    if (!auth) {
-      next({ name: 'Login' })
-    } else {
-      next()
-    }
+    !auth ? next({ name: 'Login' }) : next()
   } else {
-    if (auth) {
-      next({ name: 'Home' })
-    } else {
-      next()
-    }
+    auth ? next({ name: 'Home' }) : next()
   }
 })
 
