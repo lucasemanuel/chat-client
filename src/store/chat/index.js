22bbd4cf6331fa1b53ae1ec6aa645c1f07307a32
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-03-08 21:13:13 -0300

    Adicionando mensagem enviada na lista de mensagens

--- src/store/chat/index.js
+++ src/store/chat/index.js
@@ -2,7 +2,8 @@ import axios from '@/http/axios'
 import {
   SET_LIST_USERS,
   SET_USER_DESTINATION,
-  SET_MESSAGES
+  SET_MESSAGES,
+  ADD_MESSAGE
 } from './mutation-types'
 
 const state = {
@@ -24,6 +25,9 @@ const mutations = {
   },
   [SET_MESSAGES] (state, payload) {
     state.conversation.messages = payload
+  },
+  [ADD_MESSAGE] (state, payload) {
+    state.conversation.messages = [...state.conversation.messages, payload]
   },
   [SET_USER_DESTINATION] (state, payload) {
     state.conversation.userDestination = state.listUsers[payload]
@@ -62,6 +66,7 @@ const actions = {
       axios
         .post('/messages/', payload)
         .then(response => {
+          commit(ADD_MESSAGE, response.data)
           resolve(response)
         })
         .catch(error => {
--- src/store/chat/mutation-types.js
+++ src/store/chat/mutation-types.js
@@ -1,3 +1,4 @@
 export const SET_LIST_USERS = 'set_list_users'
 export const SET_USER_DESTINATION = 'set_user_destination'
 export const SET_MESSAGES = 'set_messages'
+export const ADD_MESSAGE = 'add_message'
