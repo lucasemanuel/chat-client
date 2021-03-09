888d596d05fb54b6255ae02f411c6b30430eec9d
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-03-09 19:46:01 -0300

    Adicionando icone no menu

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <main>
     <header>
-      <span v-on:click="toggleMenu" class="menu">menu</span>
+      <span v-on:click="toggleMenu" class="menu">
+        <font-awesome-icon :icon="faBars" />
+      </span>
       <h1 class="logo">chatSocket</h1>
     </header>
     <Sidebar v-bind:hide-menu="hideMenu" v-on:update-hide-class="toggleMenu" />
@@ -10,6 +12,8 @@
 </template>
 
 <script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import Sidebar from '@/components/Sidebar'
 import Conversation from '@/components/Conversation'
 
@@ -17,11 +21,13 @@ export default {
   name: 'Home',
   components: {
     Sidebar,
-    Conversation
+    Conversation,
+    FontAwesomeIcon
   },
   data () {
     return {
-      hideMenu: false
+      hideMenu: false,
+      faBars: faBars
     }
   },
   methods: {
@@ -42,25 +48,43 @@ main {
   header {
     display: flex;
     height: 64px;
-    line-height: 64px;
     width: 100%;
     background: $primary-color;
     grid-area: header;
     border-bottom: $border;
+    align-items: center;
+    justify-content: space-between;
 
-    .menu span {
+    .menu {
       z-index: 1;
+      color: $font-color;
+      font-size: 28px;
+      margin-left: 16px;
+      cursor: pointer;
     }
 
     h1.logo {
       font-size: 28px;
-      text-align: center;
-      width: 100%;
-      display: block;
-      position: absolute;
       text-transform: uppercase;
       letter-spacing: 4px;
-      color: #1e2933;
+      color: $font-color;
+      margin-right: 16px;
+    }
+
+    @media (min-width: 400px) {
+      justify-content: initial;
+
+      h1.logo {
+        width: 100%;
+        display: block;
+        position: absolute;
+        text-align: center;
+      }
+      //   h1 {
+      //     display: inline;
+      //     position: relative;
+      //     width: auto;
+      //   }
     }
 
     span {
