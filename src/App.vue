bd31a0172d41000b10dcc4783d8215de764ffd40
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-03-10 01:03:05 -0300

    Mostrando toast quando o status da resposta for 422

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,13 @@ import { ADD_MESSAGE } from '@/store/chat/mutation-types'
 export default {
   created () {
     this.$http.interceptors.response.use(undefined, ({ response: error }) => {
-      if (error.status === 401) {
+      if (error.status === 401 && !error.config.url.match(/(logout)/g)) {
         this.$store.dispatch('logout').then(() => {
           this.$router.push({ name: 'Login', params: { auth: false } })
         })
+      } else if (error.status === 422) {
+        const key = Object.keys(error.data.errors)[0]
+        this.$toasted.show(error.data.errors[key][0])
       } else {
         return Promise.reject(error)
       }
