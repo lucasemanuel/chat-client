d06a562af3f9cbcf8ff87026c49dd8b3163c93cf
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-03-10 01:02:13 -0300

    Configurando toast e moment.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
-import App from './App.vue'
 import Echo from 'laravel-echo'
-import router from './router'
-import store from './store'
+import Toasted from 'vue-toasted'
+import moment from 'moment'
+
 import axios from './http/axios'
+import store from './store'
+import router from './router'
+import App from './App.vue'
+
+moment.locale('pt-BR')
 
 axios.defaults.headers.common = {
   Authorization: `Bearer ${localStorage.getItem('token')}`
@@ -29,10 +34,18 @@ Vue.prototype.$echo = new Echo({
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
+Vue.use(Toasted, {
+  theme: 'bubble',
+  duration: 3000
+})
 
 Vue.filter('at', value => {
   if (!value) return ''
   return `@${value}`
+})
+
+Vue.filter('date', value => {
+  if (value) return moment(value).format('DD/MM/YYYY HH:mm')
 })
 
 new Vue({
